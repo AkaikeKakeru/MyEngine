8834ba58e18418a5d458d09f4028a9e6ec8f8d00
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-06 11:11:41 +0900

    思いつく初期化関数の枠を用意

--- DirectXBasis.cpp
+++ DirectXBasis.cpp
@@ -1,6 +1,30 @@
 #include "DirectXBasis.h"
-
 void DirectXBasis::Initialize(){
+
+	InitDevice();
+	InitCommand();
+	InitSwapChain();
+	InitRenderTargetView();
+	InitDepthBuffer();
+	InitFence();
+}
+
+void DirectXBasis::InitDevice(){
+}
+
+void DirectXBasis::InitCommand(){
+}
+
+void DirectXBasis::InitSwapChain(){
+}
+
+void DirectXBasis::InitRenderTargetView(){
+}
+
+void DirectXBasis::InitDepthBuffer(){
+}
+
+void DirectXBasis::InitFence(){
 }
 
 void DirectXBasis::Draw(){
--- DirectXBasis.h
+++ DirectXBasis.h
@@ -5,7 +5,22 @@ public://static
 public://基本関数	
 	void Initialize();
 	void Draw();
+
 public://固有関数
+	///初期化
+	//デバイス
+	void InitDevice();
+	//コマンド
+	void InitCommand();
+	//スワップチェーン
+	void InitSwapChain();
+	//レンダ―ターゲットビュー
+	void InitRenderTargetView();
+	//深度バッファ
+	void InitDepthBuffer();
+	//フェンス
+	void InitFence();
+
 public://定数
 private://変数
 };
