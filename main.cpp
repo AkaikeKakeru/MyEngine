a1ec515481aa42a3eb7cda76c83e826aae1afdc1
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-06 10:56:08 +0900

    mainでDirectXBasisのインスタンスを生成

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <Windows.h>
-#include "WinApp.h"
 #include "SafeDelete.h"
+#include "WinApp.h"
+#include "DirectXBasis.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	//基盤初期化
+	///基盤初期化
+	//アプリケーション
 	WinApp* winApp_ = new WinApp();
 	winApp_->Initialize();
 
-	//描画初期化
+	//DirectX基盤
+	DirectXBasis* dxBas_ = new DirectXBasis();
+	dxBas_->Initialize();
 
-	//ゲームループ
+	/// 描画初期化
+
+	/// ゲームループ
 	while (true) {
 		//windowsのメッセージ処理
 		if (winApp_->ProcessMessage()) {
@@ -18,13 +24,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			break;
 		}
 
-		//更新
+		/// 更新
 
-		//描画
+		/// 描画
+		dxBas_->Draw();
 	}
 
-	//解放
+	/// 解放
 	SafeDelete(winApp_);
+	SafeDelete(dxBas_);
 
 	return 0;
 }
