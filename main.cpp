5e42c22d39bc4d84e28ed432a0a977042a232d63
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-06 10:09:31 +0900

    簡単な仕切りを用意

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <Windows.h>
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	//基盤初期化
+
+	//描画初期化
+
+	//ゲームループ
+	while (true) {
+
+		//更新
+
+		//描画
+	}
+
+	//解放
+
+	return 0;
+}
