441f57e482444b258cce307801e36952422726b2
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-12 23:59:03 +0900

    初めての図形描画を復習する。まずは三角形用の頂点データ用意

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -1,4 +1,14 @@
 #include "DrawBasis.h"
+#include "Vector3.h"
+#include <wrl.h>
 
-void DrawBasis::Initialize(){
+void DrawBasis::Initialize() {
+	//頂点データ
+	Vector3 vertices[] = {
+		{-0.5f,0.5f,0.0f},//左下
+		{-0.5f,+0.5f,0.0f},//左上
+		{+0.5f,-0.5f,0.0f},//右下
+	};
+	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
+	UINT sizeVB = static_cast<UINT>(sizeof(Vector3) * _countof(vertices));
 }
