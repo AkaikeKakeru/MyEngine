f5bc08633de6f8412faef047bec805cdb3200eae
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-13 10:17:27 +0900

    頂点バッファの確保(設定)

--- DrawBasis.cpp
+++ DrawBasis.cpp
@@ -1,6 +1,12 @@
 #include "DrawBasis.h"
-#include "Vector3.h"
+#include <d3d12.h>
 #include <wrl.h>
+#include <cassert>
+#include "Vector3.h"
+
+#pragma comment(lib, "d3d12lib")
+
+//using namespace Microsoft::WRL;
 
 void DrawBasis::Initialize() {
 	//頂点データ
@@ -11,4 +17,17 @@ void DrawBasis::Initialize() {
 	};
 	//頂点データ全体のサイズ = 頂点データ一つ分のサイズ * 頂点データの要素数
 	UINT sizeVB = static_cast<UINT>(sizeof(Vector3) * _countof(vertices));
+
+	//頂点バッファの設定
+	D3D12_HEAP_PROPERTIES vbHeapProp{};
+	vbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;
+	//リソース設定
+	D3D12_RESOURCE_DESC vbResDesc{};
+	vbResDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	vbResDesc.Width = sizeVB;
+	vbResDesc.Height = 1;
+	vbResDesc.DepthOrArraySize = 1;
+	vbResDesc.MipLevels = 1;
+	vbResDesc.SampleDesc.Count = 1;
+	vbResDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
 }
