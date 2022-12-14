505f4a26d188afbb0460ca198d77686803fd3757
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-14 15:55:53 +0900

    各シェーダファイル内で、シェーダヘッダをインクルード。且つ、頂点シェーダの出力をピクセルシェーダーに渡し、ピクセルシェーダーで色付けさせる流れを作る。

--- Resource/shader/SpritePS.hlsl
+++ Resource/shader/SpritePS.hlsl
@@ -1,7 +1,5 @@
-cbuffer ConstBufferDataMaterial : register(b0) {
-	float4 color;//色(RGBA)
-}
+#include "Sprite.hlsli"
 
-float4 main() : SV_TARGET{
-	return color;
+float4 main(VSOutput input) : SV_TARGET{
+	return float4(input.uv,0,1);
 }
--- Resource/shader/SpriteVS.hlsl
+++ Resource/shader/SpriteVS.hlsl
@@ -1,3 +1,9 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION{
-	return pos;
+#include "Sprite.hlsli"
+
+VSOutput main( float4 pos : POSITION, float2 uv : TEXCOORD ){
+	VSOutput output; //ピクセルシェーダーに渡す値
+	output.svpos = pos;
+	output.uv = uv;
+
+	return output;
 }
