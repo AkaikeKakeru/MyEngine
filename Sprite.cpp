1a995af1637f455a9a1afa4e4c102ff306fb8e14
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-15 13:59:26 +0900

    定数バッファトランスフォームを生成

--- Sprite.cpp
+++ Sprite.cpp
@@ -53,7 +53,7 @@ void Sprite::GenerateConstMaterial(){
 	//定数バッファヒープ設定
 	D3D12_HEAP_PROPERTIES cbHeapProp{};
 	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;//GPUへの転送用
-											 //定数バッファリソース設定
+	//定数バッファリソース設定
 	D3D12_RESOURCE_DESC cbResourceDesc{};
 	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
 	cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial) + 0xff) & ~0xff;//256バイトアライメント
@@ -78,10 +78,43 @@ void Sprite::GenerateConstMaterial(){
 	assert(SUCCEEDED(result));
 
 	//値を書き込むと自動的に転送される
-	constMapMaterial_->color = Vector4(1, 1, 1, 1.0f);//RGBAで半透明の赤
+	constMapMaterial_->color = Vector4(1, 1, 1, 1.0f);
 }
 
 void Sprite::GenerateConstTransform(){
+	HRESULT result;
+
+	//定数バッファヒープ設定
+	D3D12_HEAP_PROPERTIES cbHeapProp{};
+	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;//GPUへの転送用
+	//定数バッファリソース設定
+	D3D12_RESOURCE_DESC cbResourceDesc{};
+	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	cbResourceDesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff;//256バイトアライメント
+	cbResourceDesc.Height = 1;
+	cbResourceDesc.DepthOrArraySize = 1;
+	cbResourceDesc.MipLevels = 1;
+	cbResourceDesc.SampleDesc.Count = 1;
+	cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	//定数バッファの生成
+	result = device_->CreateCommittedResource(
+		&cbHeapProp,//ヒープ設定
+		D3D12_HEAP_FLAG_NONE,
+		&cbResourceDesc,//リソース設定
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&constBuffTransform_));
+	assert(SUCCEEDED(result));
+
+	//定数バッファのマッピング
+	result = constBuffTransform_->Map(0, nullptr, (void**)&constMapTransform_);//マッピング
+	assert(SUCCEEDED(result));
+
+
+	//値を書き込むと自動的に転送される
+	constMapTransform_->mat = Matrix4Identity();
+
 }
 
 void Sprite::GenerateTextureBuffer(){
