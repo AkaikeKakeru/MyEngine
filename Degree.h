988b4ec6e13e97675d28ee8291bc6afc993f30ee
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-16 12:21:31 +0900

    Degreeに、今更だが.cppを追加する。

--- Degree.h
+++ Degree.h
@@ -1,10 +1,3 @@
 #pragma once
 
-float ConvertToRadian(float degree) {
-	const float PI = 3.14159265f;
-
-	float radian = 0;
-	radian = degree * (PI / 180);
-
-	return radian;
-}
+float ConvertToRadian(float degree);
