a0847ef249810ca914acc82905a651c1c01e2bd0
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-12-19 20:44:46 +0900

    Inputクラスの枠を用意

---
+++ Input.cpp
@@ -0,0 +1,7 @@
+#include "Input.h"
+
+void Input::Initialize(){
+}
+
+void Input::Update(){
+}
---
+++ Input.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "WinApp.h"
+#define DIRECTINPUT_VERSION 0x0800 //DirectInput Version
+#include <dinput.h>
+
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+
+class Input {
+public://基本の関数
+	void Initialize();
+	void Update();
+private:
+};
