06fdb22b39393785302d9ec94bc2ff13b7d6ce93
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-12 23:31:07 +0900

    ビュープロジェクションクラス

---
+++ ViewProjection.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "Vector3.h"
+#include "Vector4.h"
+#include "Matrix4.h"
+#include <d3d12.h>
+#include <wrl.h>
+#include "Degree.h"
+
+//カメラデータ構造体
+struct CameraData {
+	// 視点座標
+	Vector3 eye_;
+	// 注視点座標
+	Vector3 target_;
+	// 上方向ベクトル
+	Vector3 up_;
+};
+
+//定数バッファデータ構造体(ビュープロジェクション)
+struct ConstBufferDataViewProjection {
+	//ビュー
+	Matrix4 view_;
+	//プロジェクション
+	Matrix4 projection_;
+	//カメラ位置
+	Vector3 cameraPos_;
+};
+
+struct ViewProjection {
+	//初期化
+	void Initialize();
+
+	//定数バッファ生成
+	void CreateConstBuffer();
+
+	//マッピング
+	void Maping();
+
+	//行列の更新
+	void UpdeteMatrix();
+	//プロジェクション行列更新
+	void UpdateProjectionMatrix();
+	//ビュー行列更新
+	void UpdateViewMatrix();
+
+	// ビュー行列
+	Matrix4 matView_;
+	// 射影行列
+	Matrix4 matProjection_;
+	//垂直視野角
+	float angle_;
+	//アスペクト比
+	float aspect_;
+	//ニアクリップ
+	float nearClip_;
+	//ファークリップ
+	float farClip_;
+
+	//カメラデータ
+	CameraData camera_;
+
+	// 定数バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> constBuff_;
+	// マッピング済みアドレス
+	ConstBufferDataViewProjection* constMap_ = nullptr;
+};
