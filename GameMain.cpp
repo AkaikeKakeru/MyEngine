54b9c1bc368c8c9d6e0e044087d0e25be48af975
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-13 18:04:00 +0900

    GameMainを圧縮

--- GameMain.cpp
+++ GameMain.cpp
@@ -37,134 +37,22 @@ void GameMain::Initialize(){
 	input_ = Input::GetInstance();
 	input_->Initialize();
 
-	/// 描画初期化
-	//オブジェクト基盤
-	Object3d::StaticInitialize(dxBas_->GetDevice().Get(), WinApp::Win_Width, WinApp::Win_Height);
-
-	//オブジェクトモデル
-
-	model_ = Model::LoadFromOBJ("cube");
-	model_2 = Model::LoadFromOBJ("triangle_mat");
-	modelSkydome_ = Model::LoadFromOBJ("skydome");
-	modelGround_ = Model::LoadFromOBJ("ground");
-
-	object3d_ = Object3d::Create();
-	object3d_2 = Object3d::Create();
-	skydome_ = Object3d::Create();
-	ground_ = Object3d::Create();
-
-	object3d_->SetModel(model_);
-	object3d_2->SetModel(model_2);
-
-	skydome_->SetModel(modelSkydome_);
-	ground_->SetModel(modelGround_);
-
-	object3d_->SetPosition({ -5.0f, 0.0f, -5.0f });
-	object3d_2->SetPosition({ +5.0f, 0.0f, +5.0f });
-
-	//描画基盤
-	drawBas_ = DrawBasis::GetInstance();
-	drawBas_->Initialize();
-
-	drawBas_->LoadTexture(0, "smile.png");
-	drawBas_->LoadTexture(1, "texture.png");
-
-	//描画スプライト
-
-	sprite_->Initialize(drawBas_,0);
-	sprite2_->Initialize(drawBas_,0);
-	sprite3_->Initialize(drawBas_,1);
+	//シーン
+	scene_ = new GamePlayScene();
+	scene_->Initialize(dxBas_);
 }
 
 void GameMain::Update(){
 
 	/// 更新
-	input_->Update();
-
-	Vector2 pos = { 0,0 };
-
-	//オブジェクト更新
-
-	//オブジェクト移動
-	if (input_->PressKey(DIK_UP) || input_->PressKey(DIK_DOWN) || input_->PressKey(DIK_RIGHT) || input_->PressKey(DIK_LEFT)) {
-		// 現在の座標を取得
-		Vector3 position = object3d_->GetPosition();
-
-		// 移動後の座標を計算
-		if (input_->PressKey(DIK_UP)) { position.y += 1.0f; }
-		else if (input_->PressKey(DIK_DOWN)) { position.y -= 1.0f; }
-		if (input_->PressKey(DIK_RIGHT)) { position.x += 1.0f; }
-		else if (input_->PressKey(DIK_LEFT)) { position.x -= 1.0f; }
-
-		// 座標の変更を反映
-		object3d_->SetPosition(position);
-	}
-
-	// カメラ移動
-	if (input_->PressKey(DIK_W) || input_->PressKey(DIK_S) || input_->PressKey(DIK_D) || input_->PressKey(DIK_A)) {
-		if (input_->PressKey(DIK_W)) { Object3d::CameraMoveEyeVector({ 0.0f,+1.0f,0.0f }); }
-		else if (input_->PressKey(DIK_S)) { Object3d::CameraMoveEyeVector({ 0.0f,-1.0f,0.0f }); }
-		if (input_->PressKey(DIK_D)) { Object3d::CameraMoveEyeVector({ +1.0f,0.0f,0.0f }); }
-		else if (input_->PressKey(DIK_A)) { Object3d::CameraMoveEyeVector({ -1.0f,0.0f,0.0f }); }
-	}
-
-	skydome_->Update();
-	ground_->Update();
-	object3d_->Update();
-	object3d_2->Update();
-
-	pos = sprite_->GetPosition();
-	if (input_->PressMouse(LeftButton)) {
-		pos.x += 2.0f;
-	}
-	sprite_->SetPosition(pos);
-	sprite_->SetRotation(ConvertToRadian(45.0f));
-	sprite_->SetColor(Vector4(0.5f, 1.0f, 0.2f, 0.4f));
-	sprite_->SetSize(Vector2(80, 120));
-	sprite_->SetIsFlipX(true);
-	sprite_->Update();
-
-	pos = sprite2_->GetPosition();
-	if (input_->TriggerMouse(CenterButton)
-		|| input_->PressMouse(RightButton)) {
-		pos.y += 2.0f;
-	}
-	sprite2_->SetPosition(pos);
-	sprite2_->SetColor(Vector4(0.8f, 0.2f, 0.5f, 0.9f));
-	sprite2_->SetSize(Vector2(140, 50));
-	sprite2_->SetIsFlipY(true);
-	sprite2_->Update();
-
-	sprite3_->SetPosition(input_->GetMousePosition());
-	sprite3_->SetAnchorPoint(Vector2(0.5f, 0.5f));
-	sprite3_->SetSize({ 100,100 });
-	sprite3_->SetTextureSize({256,256});
-	sprite3_->Update();
+	scene_->Update(input_);
 }
 
 void GameMain::Draw(){
 	//描画前処理
 	dxBas_->PreDraw();
 
-	//モデル本命処理
-	Object3d::PreDraw(dxBas_->GetCommandList().Get());
-
-	skydome_->Draw();
-	ground_->Draw();
-
-	object3d_2->Draw();
-	object3d_->Draw();
-
-	Object3d::PostDraw();
-
-	//スプライト本命処理
-	drawBas_->PreDraw();
-
-	sprite_->Draw();
-	sprite2_->Draw();
-	sprite3_->Draw();
-
-	drawBas_->PostDraw();
+	scene_->Draw(dxBas_);
 
 	//描画後処理
 	dxBas_->PostDraw();
@@ -172,16 +60,7 @@ void GameMain::Draw(){
 
 void GameMain::Finalize(){
 	/// 解放
-	SafeDelete(skydome_);
-	SafeDelete(ground_);
-	SafeDelete(object3d_);
-	SafeDelete(object3d_2);
-	SafeDelete(model_);
-	SafeDelete(model_2);
-	SafeDelete(modelSkydome_);
-	SafeDelete(modelGround_);
+	scene_->Finalize();
 
-	SafeDelete(sprite_);
-	SafeDelete(sprite2_);
-	SafeDelete(sprite3_);
+	delete scene_;
 }
--- GameMain.h
+++ GameMain.h
@@ -4,12 +4,12 @@
 #include "WinApp.h"
 #include "DirectXBasis.h"
 #include "Input.h"
-#include "ObjectBasis.h"
 #include "DrawBasis.h"
 #include "Model.h"
 #include "Sprite.h"
-#include "Degree.h"
 #include "Object3d.h"
+#include "Degree.h"
+#include "GamePlayScene.h"
 
 class GameMain {
 public://構造体
@@ -34,27 +34,6 @@ private://変数
 
 	bool isEndRequest_ = false;
 
-	/// <summary>
-	/// オブジェクト
-	/// </summary>
-
-	Model* model_ = nullptr;
-	Model* model_2 = nullptr;
-	Model* modelSkydome_ = nullptr;
-	Model* modelGround_ = nullptr;
-
-	Object3d* object3d_ = nullptr;
-	Object3d* object3d_2 = nullptr;
-	Object3d* skydome_ = nullptr;
-	Object3d* ground_ = nullptr;
-
-	/// <summary>
-	/// スプライト
-	/// </summary>
-
-	DrawBasis* drawBas_ = nullptr;
-
-	Sprite* sprite_ = new Sprite();
-	Sprite* sprite2_ = new Sprite();
-	Sprite* sprite3_ = new Sprite();
+	//シーン
+	GamePlayScene* scene_ = nullptr;
 };
