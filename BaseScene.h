e786a603b5c912d4a9501ee3257550d6e1be41d7
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-13 18:46:38 +0900

    シーンマネージャーと、ベースシーンを用意

---
+++ BaseScene.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class BaseScene {
+public:
+	virtual void Initialize() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+	virtual void Finalize() = 0;
+public:
+	virtual ~BaseScene() = default;
+};
