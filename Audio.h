7cf5d5a057aac3be6da4b826d3c101aa1cd614e5
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-31 17:41:59 +0900

    構造体を定義

--- Audio.h
+++ Audio.h
@@ -1,7 +1,36 @@
 #pragma once
 
 class Audio {
-public:
+public://構造体
+	//チャンクヘッダ
+	struct ChunkHeader {
+		char id_[4];//チャンクのID
+		int32_t size_;//チャンクサイズ
+	};
+
+	//RIFFヘッダファイル
+	struct RiffHeader {
+		ChunkHeader chunk_;//"RIFF"
+		char type_[4];//"WAVE"
+	};
+
+	//FMTチャンク
+	struct FormatChunk {
+		ChunkHeader chunk_;//"FMT"
+		WAVEFORMAT fmt_;//波形フォーマット
+	};
+
+	//音声データ
+	struct SoundData {
+		//波形フォーマット
+		WAVEFORMAT wfex_;
+		//バッファの先頭アドレス
+		BYTE* pBuffer_;
+		//バッファのサイズ
+		unsigned int bufferSize_;
+	};
+
+public://メンバ関数
 	void Initialize();
 
 private:
@@ -13,5 +42,4 @@ private:
 	const Audio& operator=(const Audio&) = delete;
 public:
 	static Audio* GetInstance();
-
 };
