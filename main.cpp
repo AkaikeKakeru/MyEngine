6868e6a511e49c44021df1e4cede74217ad0ca50
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-31 11:54:47 +0900

    実行確認

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include "Framework.h"
 #include "GameMain.h"
+#include "ImGuiManager.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-
+	ImGuiManager* imGuiManager_ = new ImGuiManager();
+	imGuiManager_->Initialize();
 
 	FrameworkΓ* game = new GameMain();
-
 	game->Run();
 
+	delete imGuiManager_;
 	delete game;
-
 	return 0;
 }
