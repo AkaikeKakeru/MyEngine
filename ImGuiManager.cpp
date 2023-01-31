61c8ddca3124d6f2a513bd6ae9a23300b024d776
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-31 14:09:03 +0900

    DirectX12用初期化

--- ImGuiManager.cpp
+++ ImGuiManager.cpp
@@ -1,5 +1,6 @@
 #include "ImGuiManager.h"
 #include <imgui_impl_win32.h>
+#include <imGui_impl_DX12.h>
 #include "WinApp.h"
 
 ImGuiManager* ImGuiManager::GetInstance() {
@@ -22,9 +23,17 @@ void ImGuiManager::Initialize(DirectXBasis* dXBas) {
 	desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
 	desc.NumDescriptors = 1;
 	desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-	
+
 	//デスクリプタヒープ生成
 	result = dXBas->GetDevice()->CreateDescriptorHeap(
 		&desc, IID_PPV_ARGS(&srvHeap_));
 	assert(SUCCEEDED(result));
+
+	ImGui_ImplDX12_Init(
+		dXBas->GetDevice().Get(),
+		static_cast<int>(dXBas->GetBackBufferCount()),
+		DXGI_FORMAT_R8G8B8A8_UNORM_SRGB, srvHeap_.Get(),
+		srvHeap_->GetCPUDescriptorHandleForHeapStart(),
+		srvHeap_->GetGPUDescriptorHandleForHeapStart()
+	);
 }
