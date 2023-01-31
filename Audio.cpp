c2d7ad04aff2ef3b33083d48b58a754bbb5ace9a
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-31 17:34:15 +0900

    エンジンのインスタンスを呼び出し、マスタリングボイスの生成

--- Audio.cpp
+++ Audio.cpp
@@ -1,16 +1,29 @@
 #include "Audio.h"
 #include <xaudio2.h>
+#include <wrl.h>
+
+#include <fstream>
+#include <cassert>
+
+#pragma comment(lib,"xaudio2.lib")
+
 
 //省略
 template<class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 
-#pragma comment(lib,"xaudio.2lib")
-
-#include <fstream>
-
 void Audio::Initialize() {
+	HRESULT result;
+
 	ComPtr<IXAudio2> xAudio2;
 	IXAudio2MasteringVoice* masterVoice;
+
+	//XAudioエンジンのインスタンス生成
+	result = XAudio2Create(&xAudio2, 0, XAUDIO2_DEFAULT_PROCESSOR);
+	SUCCEEDED(result);
+	
+	//マスタリングボイス生成
+	result = xAudio2->CreateMasteringVoice(&masterVoice);
+	SUCCEEDED(result);
 }
 
 Audio* Audio::GetInstance() {
