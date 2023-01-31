9309be79b62996a4b76164c23f66664c8f052aac
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-31 11:54:10 +0900

    初期化処理追加

--- ImGuiManager.cpp
+++ ImGuiManager.cpp
@@ -1,1 +1,12 @@
 #include "ImGuiManager.h"
+#include <imgui_impl_win32.h>
+#include "WinApp.h"
+
+void ImGuiManager::Initialize() {
+	//コンテキストを生成
+	ImGui::CreateContext();
+	//スタイルを設定
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplWin32_Init(WinApp::GetInstance()->GetHWND());
+}
--- ImGuiManager.h
+++ ImGuiManager.h
@@ -1,5 +1,6 @@
 #pragma once
 
 class ImGuiManager {
-
+public:
+	void Initialize();
 };
