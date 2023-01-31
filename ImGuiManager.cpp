929da23bb0aea49c1898d08fc32a18082acd8f7d
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-01-31 14:02:50 +0900

    デスクリプタヒープ生成

--- ImGuiManager.cpp
+++ ImGuiManager.cpp
@@ -8,10 +8,23 @@ ImGuiManager* ImGuiManager::GetInstance() {
 }
 
 void ImGuiManager::Initialize(DirectXBasis* dXBas) {
+	HRESULT result;
+
 	//コンテキストを生成
 	ImGui::CreateContext();
 	//スタイルを設定
 	ImGui::StyleColorsDark();
 
 	ImGui_ImplWin32_Init(WinApp::GetInstance()->GetHWND());
+
+	//デスクリプタヒープ設定
+	D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+	desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	desc.NumDescriptors = 1;
+	desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	
+	//デスクリプタヒープ生成
+	result = dXBas->GetDevice()->CreateDescriptorHeap(
+		&desc, IID_PPV_ARGS(&srvHeap_));
+	assert(SUCCEEDED(result));
 }
--- ImGuiManager.h
+++ ImGuiManager.h
@@ -2,6 +2,10 @@
 #include "DirectXBasis.h"
 
 class ImGuiManager {
+private://省略
+	template <class T>
+	using ComPtr = Microsoft::WRL::ComPtr<T>;
+
 public:
 	void Initialize(DirectXBasis* dXBas);
 private:
@@ -11,6 +15,10 @@ private:
 	ImGuiManager(const ImGuiManager&) = delete;
 	//代入演算子を無効
 	const ImGuiManager& operator=(const ImGuiManager&) = delete;
+
+public:
+	//SRV用デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap> srvHeap_;
 
 public://static
 	static ImGuiManager* GetInstance();
