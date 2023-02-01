292cc8e0fa0e0033c57e043678323ec5faa92266
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-02-01 11:24:27 +0900

    オブジェクトをQuaternionで回転させる

--- Application/Game/Scene/GamePlayScene.cpp
+++ Application/Game/Scene/GamePlayScene.cpp
@@ -1,5 +1,6 @@
 #include "GamePlayScene.h"
 #include "SafeDelete.h"
+#include "Quaternion.h"
 
 DirectXBasis* GamePlayScene::dxBas_ = DirectXBasis::GetInstance();
 Input* GamePlayScene::input_ = Input::GetInstance();
@@ -36,13 +37,21 @@ void GamePlayScene::Initialize3d() {
 	planeModel_ = Model::LoadFromOBJ("plane", true);
 
 	skydomeModel_ = new Model();
-	skydomeModel_ = Model::LoadFromOBJ("skydome",false);
-
+	skydomeModel_ = Model::LoadFromOBJ("skydome", false);
 
 	planeObj_ = new Object3d();
 	planeObj_ = Object3d::Create();
 	planeObj_->SetModel(planeModel_);
 	planeObj_->SetScale({ 1, 1, 1 });
+
+	//回転軸アングル
+	Quaternion rotation = MakeAxisAngle(
+		{ 0.0f,1.0f,0.0f }, ConvertToRadian(1.0f));
+
+	//1F当たりの回転角度
+	Vector3 point = { 0.0f,ConvertToRadian(180.0f),0.0f };
+
+	planeObj_->SetRotation(RotateVector(point, rotation));
 
 	skydomeObj_ = new Object3d();
 	skydomeObj_ = Object3d::Create();
@@ -62,42 +71,89 @@ void GamePlayScene::Initialize2d() {
 
 	sprite_->SetAnchorPoint({ 0.5f, 0.5f });
 	sprite_->SetSize({ 64,64 });
-
 }
 
 void GamePlayScene::Update3d() {
-	skydomeObj_->Update();
-	planeObj_->Update();
+	// オブジェクト回転
+	{
+		// 現在の座標を取得
+		Vector3 rot = skydomeObj_->GetRotation();
+
+		// 移動後の座標を計算
+
+		//回転軸アングル
+		//ここではY軸回転を指定
+		Quaternion rotation = MakeAxisAngle(
+			{ 0.0f,1.0f,0.0f }, ConvertToRadian(1.0f));
+
+		//1F当たりの回転角度を指定
+		Vector3 pointY = { 0.0f,ConvertToRadian(1.0f),0.0f };
+
+		rot += RotateVector(pointY, rotation);
+
+		// 座標の変更を反映
+		skydomeObj_->SetRotation(rot);
+	}
 
 	// オブジェクト移動
 	if (input_->PressKey(DIK_UP) ||
-		input_->PressKey(DIK_DOWN) || 
-		input_->PressKey(DIK_RIGHT) || 
+		input_->PressKey(DIK_DOWN) ||
+		input_->PressKey(DIK_RIGHT) ||
 		input_->PressKey(DIK_LEFT))
 	{
 		// 現在の座標を取得
 		Vector3 position = planeObj_->GetPosition();
-
-		//移動後の座標を計算
-		if (input_->PressKey(DIK_UP)) { position.y += 0.5f; }
-		else if (input_->PressKey(DIK_DOWN)) { position.y -= 0.5f; }
-		if (input_->PressKey(DIK_RIGHT)) { position.x += 0.5f; }
-		else if (input_->PressKey(DIK_LEFT)) { position.x -= 0.5f; }
-
-		// 座標の変更を反映
-		planeObj_->SetPosition(position);
-	}
-
-	// オブジェクト回転
-	{
 		// 現在の座標を取得
 		Vector3 rot = planeObj_->GetRotation();
 
-		// 移動後の座標を計算
-		rot.y += ConvertToRadian(1.0f);
+		//回転軸アングル
+		Quaternion rotation = MakeAxisAngle(
+			{ 0.0f,0.0f,0.0f }, ConvertToRadian(1.0f));
+
+		//1F当たりの回転角度
+		Vector3 point = { 0.0f,0.0f,0.0f };
+
+		//移動後の座標を計算
+		if (input_->PressKey(DIK_UP)) {
+			// 移動後の座標を計算
+			position.y += 0.5f;
+
+			rotation = MakeAxisAngle(
+				{ 1.0f,0.0f,0.0f }, ConvertToRadian(1.0f));
+			point = { ConvertToRadian(1.0f), 0.0f,0.0f };
+		}
+
+		else if (input_->PressKey(DIK_DOWN)) {
+			position.y -= 0.5f;
+
+			rotation = MakeAxisAngle(
+				{ 1.0f,0.0f,0.0f }, ConvertToRadian(1.0f));
+			point = { -ConvertToRadian(1.0f), 0.0f,0.0f };
+		}
+
+		if (input_->PressKey(DIK_RIGHT)) {
+			position.x += 0.5f;
+
+			rotation = MakeAxisAngle(
+				{ 0.0f,0.0f,1.0f }, ConvertToRadian(1.0f));
+			point = { 0.0f,0.0f,ConvertToRadian(1.0f) };
+		}
+
+		else if (input_->PressKey(DIK_LEFT)) {
+			position.x -= 0.5f;
+
+			rotation = MakeAxisAngle(
+				{ 0.0f,0.0f,1.0f }, ConvertToRadian(1.0f));
+			point = { 0.0f,0.0f,-ConvertToRadian(1.0f) };
+		}
+
+		rot += RotateVector(point, rotation);
 
 		// 座標の変更を反映
 		planeObj_->SetRotation(rot);
+
+		// 座標の変更を反映
+		planeObj_->SetPosition(position);
 	}
 
 	{
@@ -108,16 +164,15 @@ void GamePlayScene::Update3d() {
 			input_->PressKey(DIK_D) ||
 			input_->PressKey(DIK_A)) {
 			if (input_->PressKey(DIK_W)) { lightDir.y += 1.0f; }
-			else if (input_->PressKey(DIK_S)) {lightDir.y -= 1.0f; }
+			else if (input_->PressKey(DIK_S)) { lightDir.y -= 1.0f; }
 			if (input_->PressKey(DIK_D)) { lightDir.x += 1.0f; }
 			else if (input_->PressKey(DIK_A)) { lightDir.x -= 1.0f; }
 		}
-
 		light_->SetLightDir(lightDir);
-
 	}
 
 	light_->Update();
+	skydomeObj_->Update();
 	planeObj_->Update();
 }
 
@@ -126,7 +181,7 @@ void GamePlayScene::Update2d() {
 	Vector2 position = input_->GetMousePosition();
 
 	//移動後の座標を計算
-	
+
 	// 座標の変更を反映
 	sprite_->SetPosition(position);
 
