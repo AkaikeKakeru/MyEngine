384b942bd5d2e297e82f07b5b2db130d4b6fbdcf
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-04-20 12:02:21 +0900

    実行確認が取れたので、FBXマネージャー生成を削除

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include "Framework.h"
 #include "GameMain.h"
-#include "fbxsdk.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	FbxManager* fbxManager = FbxManager::Create();
-
 	Framework* game = new GameMain();
 	game->Run();
 
