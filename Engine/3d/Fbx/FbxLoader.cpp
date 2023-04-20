38047baa153c5a76e0935a50fa605a86597fd9f3
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-04-20 12:35:45 +0900

    FBXSDKの、初期化と後始末を定義

--- Engine/3d/Fbx/FbxLoader.cpp
+++ Engine/3d/Fbx/FbxLoader.cpp
@@ -1,7 +1,27 @@
 ﻿#include "FbxLoader.h"
+#include <cassert>
 
-FbxLoader* FbxLoader::GetInstance()
-{
+FbxLoader* FbxLoader::GetInstance() {
     static FbxLoader instance;
     return &instance;
+}
+
+void FbxLoader::Initialize(ID3D12Device* device) {
+    // 再初期化チェック
+    assert(fbxManager_ == nullptr);
+    // 引数からメンバ変数に代入
+    device_ = device;
+    // FBXマネージャーの生成
+    fbxManager_ = FbxManager::Create();
+    // FBXマネージャーの入出力設定
+    FbxIOSettings* ios = FbxIOSettings::Create(fbxManager_, IOSROOT);
+    fbxManager_->SetIOSettings(ios);
+    // FBXインポータの生成
+    fbxImporter_ = FbxImporter::Create(fbxManager_, "");
+}
+
+void FbxLoader::Finalize() {
+    // 各種FBXインスタンスの破棄
+    fbxImporter_->Destroy();
+    fbxManager_->Destroy();
 }
