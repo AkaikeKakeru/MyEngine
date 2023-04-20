58cca5a1f3c190132c8703e0e8bd75934803aab6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-04-20 12:01:36 +0900

    FBXManager実行確認

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "Framework.h"
 #include "GameMain.h"
+#include "fbxsdk.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	FbxManager* fbxManager = FbxManager::Create();
+
 	Framework* game = new GameMain();
 	game->Run();
 
