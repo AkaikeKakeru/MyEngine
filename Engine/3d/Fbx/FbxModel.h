84f1ff5b9ed9e9fbfd361dc37503dec79b4e5dd3
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-04-25 10:09:14 +0900

    ノード構造体を拡張し、配列としてモデルに持たせる

--- Engine/3d/Fbx/FbxModel.h
+++ Engine/3d/Fbx/FbxModel.h
@@ -1,9 +1,30 @@
 #pragma once
 #include <string>
+#include "Vector3.h"
+#include "Matrix4.h"
 
 //ノード構造体
 struct Node {
+	//名前
+	std::string name_;
 
+	//ローカルスケール
+	Vector3 scaling_ = { 1,1,1 };
+
+	//ローカル回転
+	Vector3 rotation_ = { 0,0,0 };
+
+	//ローカル移動
+	Vector3 translation_ = { 0,0,0 };
+
+	//ローカル変形行列
+	Matrix4 transform_;
+
+	//グローバル変形行列
+	Matrix4 globalTransform_;
+
+	//親ノード
+	Node* parent_ = nullptr;
 };
 
 //Fbxモデル
@@ -15,4 +36,6 @@ public:
 private:
 	//モデル名
 	std::string name_;
+	//ノード配列
+	std::vector<Node> nodes_;
 };
