83a3ca0391435d034841c79be4ae3873e6d83f1f
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-04-27 13:04:58 +0900

    01_08で追加したヘッダとリソース

--- Engine/3d/Fbx/FbxModel.h
+++ Engine/3d/Fbx/FbxModel.h
@@ -3,6 +3,7 @@
 #include "Vector3.h"
 #include "Matrix4.h"
 #include <vector>
+#include <DirectXTex.h>
 
 //ノード構造体
 struct Node {
@@ -34,9 +35,32 @@ public:
 	//フレンドクラス
 	friend class FbxLoader;
 
+public://サブクラス
+	//頂点データ構造体
+	struct VertexPosNormalUv {
+		Vector3 pos_;
+		Vector3 normal_;
+		Vector3 uv_;
+	};
+
 private:
 	//モデル名
 	std::string name_;
 	//ノード配列
 	std::vector<Node> nodes_;
+	//メッシュを持つノード
+	Node* meshNode = nullptr;
+	//頂点データ配列
+	std::vector<VertexPosNormalUv> vertices_;
+	//頂点インデックス配列
+	std::vector<unsigned short> indices_;
+
+	//アンビエント係数
+	Vector3 ambient_ = { 1,1,1 };
+	//ディフューズ係数
+	Vector3 diffuse_ = { 1,1,1 };
+	//テクスチャメタデータ
+	DirectX::TexMetadata metadata = {};
+	//スクラッチイメージ
+	DirectX::ScratchImage scratchImg_ = {};
 };
