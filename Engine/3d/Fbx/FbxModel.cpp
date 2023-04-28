1b4510fb9215d7f764613936b7ae71da9d1ff473
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-04-28 14:35:33 +0900

    ヒーププロパティとリソースデスクを設定し、頂点バッファを生成

--- Engine/3d/Fbx/FbxModel.cpp
+++ Engine/3d/Fbx/FbxModel.cpp
@@ -1,5 +1,31 @@
 #include "FbxModel.h"
 
 void FbxModel::CreateBuffers(ID3D12Device* device) {
+	HRESULT result;
+	//頂点データ全体のサイズ
+	UINT sizeVB = static_cast<UINT>(sizeof(VertexPosNormalUv) * vertices_.size());
 
+	// ヒーププロパティ
+	D3D12_HEAP_PROPERTIES heapProps{};
+	heapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+
+	// リソース設定
+	D3D12_RESOURCE_DESC resDesc{};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = sizeVB;
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	// 頂点バッファ生成
+	result = device->CreateCommittedResource(
+		&heapProps,
+		D3D12_HEAP_FLAG_NONE,
+		&resDesc, 
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&vertBuff_));
+	assert(SUCCEEDED(result));
 }
