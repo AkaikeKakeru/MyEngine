e6c780613c2d58fa73c74690cb629b10df83d4f6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-07-13 17:00:28 +0900

    描画処理を定義

--- Engine/2d/PostEffect.cpp
+++ Engine/2d/PostEffect.cpp
@@ -1,6 +1,32 @@
 #include "PostEffect.h"
 
-void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList) {
+void PostEffect::Draw() {
+	//非表示
+	if (isInvisible_) {
+		return;
+	}
+
+	//パイプラインステート設定
+	cmdList_->SetPipelineState(drawBas_->GetPipelineState().Get());
+	//ルートシグネチャ設定
+	cmdList_->SetGraphicsRootSignature(drawBas_->GetRootSignature().Get());
+	//プリミティブ形状設定
+	cmdList_->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+
+	//描画用テクスチャコマンド
+	drawBas_->SetTextureCommand(textureIndex_);
+
+	//頂点バッファビューの設定コマンド
+	cmdList_->IASetVertexBuffers(0, 1, &vbView_);
+
+	//定数バッファビュー(CBV)の設定コマンド
+	cmdList_->SetGraphicsRootConstantBufferView(0, constBuffMaterial_->GetGPUVirtualAddress());
+
+	//定数バッファビュー(CBV)の設定コマンド
+	cmdList_->SetGraphicsRootConstantBufferView(2, constBuffTransform_->GetGPUVirtualAddress());
+
+	//インスタンス描画
+	cmdList_->DrawInstanced(kVerticesNum, 1, 0, 0);
 }
 
 PostEffect::PostEffect()
