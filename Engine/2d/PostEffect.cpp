b1074a390aa4f54ae1b0b0768589ab575394001e
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-07-13 13:12:45 +0900

    コンストラクタ内で、内包スプライトの初期数値設定

--- Engine/2d/PostEffect.cpp
+++ Engine/2d/PostEffect.cpp
@@ -3,5 +3,13 @@
 void PostEffect::Draw(ID3D12GraphicsCommandList* cmdList) {
 }
 
-PostEffect::PostEffect() {
+PostEffect::PostEffect()
+	:Sprite(
+		100,
+		{ 0, 0 },
+		{ 500.0f, 500.0f },
+		{ 1,1,1,1 },
+		{ 0.0f, 0.0f },
+		false,
+		false) {
 }
