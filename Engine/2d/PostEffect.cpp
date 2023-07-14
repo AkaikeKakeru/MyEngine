09449e587919f9cfe5d807da18675975010692b4
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-07-14 14:29:29 +0900

    テクスチャバッファの生成

--- Engine/2d/PostEffect.cpp
+++ Engine/2d/PostEffect.cpp
@@ -1,11 +1,41 @@
 ﻿#include "PostEffect.h"
+#include "WinApp.h"
 #include <cassert> 
+#include <d3dx12.h>
 
 void PostEffect::Initialize() {
-	HRESULT result;
+	HRESULT result = 0;
 
 	//スプライト初期化
 	Sprite::Initialize();
+
+	//テクスチャバッファヒープ設定
+	D3D12_HEAP_PROPERTIES texHeapProp{};
+	texHeapProp.Type = D3D12_HEAP_TYPE_CUSTOM;
+	texHeapProp.CPUPageProperty =
+		D3D12_CPU_PAGE_PROPERTY_WRITE_BACK;
+	texHeapProp.MemoryPoolPreference = D3D12_MEMORY_POOL_L0;
+
+	//テクスチャリソース設定
+	D3D12_RESOURCE_DESC texResDesc{};
+	texResDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	texResDesc.Width = WinApp::Win_Width;
+	texResDesc.Height = (UINT)WinApp::Win_Height;
+	texResDesc.DepthOrArraySize = 1;
+	texResDesc.MipLevels = 0;
+	texResDesc.SampleDesc.Count = 1;
+	texResDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	//テクスチャバッファ生成
+	result = device_->CreateCommittedResource(
+		&texHeapProp,
+		D3D12_HEAP_FLAG_NONE,
+		&texResDesc,
+		D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+		nullptr,
+		IID_PPV_ARGS(&texBuff_));
+
+	assert(SUCCEEDED(result));
 }
 
 void PostEffect::Draw() {
