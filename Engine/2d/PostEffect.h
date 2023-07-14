391dd62c242e7383ed54fe7b94a3c137b14f3dbc
KakeruAkaike <k021g1137@g.neec.ac.jp>
2023-07-14 14:05:14 +0900

    ポストエフェクトのヘッダ拡張

--- Engine/2d/PostEffect.h
+++ Engine/2d/PostEffect.h
@@ -2,9 +2,22 @@
 #include "Sprite.h"
 class PostEffect :
     public Sprite {
+private://省略
+    template <class T>
+    using ComPtr = Microsoft::WRL::ComPtr<T>;
+
 public://メンバ関数
+    //初期化
+    void Initialize();
     //描画コマンド実行
     void Draw();
+
+private://メンバ変数
+    //テクスチャバッファ
+    ComPtr<ID3D12Resource> texBuff_;
+    //SRV用デスクリプタヒープ
+    ComPtr<ID3D12DescriptorHeap> descHeapSRV_;
+
 public://コンストラクタ
     PostEffect();
 };
